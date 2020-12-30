6d47bb1fce57483c0a78a994ddadd038a6c6b50f
Gayangi Seneviratne <sgayangi@gmail.com>
2020-12-31 01:12:48 +0530

    customer route implementation

--- startup/routes.js
+++ startup/routes.js
@@ -4,6 +4,7 @@ const customers = require('../routes/customers');
 module.exports = function (app) {
 
     app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
     //endpoints added
     app.use('/customers', customers);
