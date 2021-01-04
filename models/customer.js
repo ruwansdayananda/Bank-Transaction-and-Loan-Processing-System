c90c6328e332c1fdf20268ac086bdde206602404
ruwansdayananda <ruwansampathdayananda@gmail.com>
2021-01-04 13:38:49 +0530

    Added corporate customer

--- models/customer.js
+++ models/customer.js
@@ -30,40 +30,28 @@ function validateCustomer(customer) {
 function validateCorporate(customer) {
     const schema = Joi.object({
 
-        "individual_id": Joi.string()
+        "corporate_id": Joi.string()
             // .pattern(new RegExp('^SC.*SCR$')) //this regex means that the customer id has to start with SC and end with SCR with total 15 characters
-            .required().min(15)
+            .required().min(20)
             .messages({         //how to define custom messages
-                'string.base': `"customer_id" should be text`,
-                'string.empty': `"customer_id" cannot be an empty field`,
+                'string.base': `"corporate_id" should be text`,
+                'string.empty': `"corporate_id" cannot be an empty field`,
                 'string.regex': `"asdasda`,
-                'string.min': `"customer_id" should have a minimum length of 15`,
-                'any.required': `"customer_id" is a required field`
+                'string.min': `"corporate_id" should have a minimum length of 15`,
+                'any.required': `"corporate_id" is a required field`
             }),
-        
-        "full_name": Joi.string(30).pattern(new RegExp('^[a-z]+(?: [a-z]+)+$')).required().min(5),    //the name must have at least two words seperated by a space
-        "address": Joi.string(50).required(),
-        "national_ID": Joi.string().required().min(10),
-        "date_of_birth": Joi.date().greater('1974-01-01').less('2003-12-31').required(),
-        "residential_contact_no": Joi.string().required().min(10),
-        "personal_contact_no": Joi.string().required().min(10),
-        "date_joined": Joi.string().required(),
-        "email_address": Joi.string(30).required(),
+
+        "company_registration_number": Joi.string(40).required(),
+        "company_name":Joi.string(20).min(3).required(),
+        "company_email_address": Joi.string(30).max(319).required(),
+        "address":Joi.string(50).required(),
+        "date_of_establishment":Joi.date().required(),    //Constranins must be checked
+        "contact_no":Joi.string().required().min(10),
+        "date_joined":Joi.date().required(),     // Constraints must be checked
+        "correspondent":Joi.string(30).alphanum().required(),
+        "correspondent_email_address":Joi.string(30).max(319).required(),
         "password": Joi.string().min(5).max(1024).required(),
-        //Added for creation of bank accounts related to a company/ club
-        "company_id": Joi.string()
-
-            .required().min(15)
-            .messages({ 
-                'string.base': `"company_id" should be text`,
-                'string.empty': `"company_id" cannot be an empty field`,
-                'string.regex': `"asdasda`,
-                'string.min': `"company_id" should have a minimum length of 15`,
-                'any.required': `"company_id" is a required field`
-            }),
-
-        "date_of_est" : Joi.date().required(),
-        "head_office" : Joi.string().min(5)
+        
     });
     return schema.validate(customer);
 }
--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Customer, validateCustomer } = require('../models/customer');
+const { Customer, validateCustomer, validateCorporate } = require('../models/customer');
 const { request } = require('express');
 var path = require("path");
 
