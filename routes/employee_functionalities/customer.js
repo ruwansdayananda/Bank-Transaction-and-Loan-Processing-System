d511e6520b8415a2450ef92eb592eadf27f032b9
Gayangi Seneviratne <sgayangi@gmail.com>
2021-01-05 09:15:23 +0530

    tentative project structure updates

---
+++ routes/employee_functionalities/customer.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const { Customer,validateIndividual,validateCorporate } = require('../models/customer');
+const { pool } = require('../startup/mysql_database');
+const {request} = require('express');
+var path = require("path");
+
+router.post('/individual', (request, response) => {
+    const {
+        error
+    } = validateIndividual(request.body);
+    if (error) {
+        return response.status(404).send(error.details[0].message);
+    }
+    console.log(request);
+
+    const insert_customer = new Promise((resolve, reject) => {
+        const query = pool.query('INSERT INTO customer VALUES (?, ?)',
+            [
+                request.body.individual_id,
+                "Individual"
+            ],
+            function (error, results, fields) {
+                if (error) reject(error);
+                else resolve(results);
+            });
+    });
+    const insert_individual_customer = new Promise((resolve, reject) => {
+        const query = pool.query('INSERT INTO individual_customer VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [
+                request.body.individual_id,
+                request.body.full_name,
+                request.body.address,
+                request.body.national_ID,
+                request.body.date_of_birth,
+                request.body.residential_contact_no,
+                request.body.personal_contact_no,
+                request.body.date_joined,
+                request.body.email_address,
+                request.body.password
+            ],
+            function (error, results, fields) {
+                if (error) reject(error);
+                else resolve(results);
+            });
+    });
+    insert_customer
+        .then(result => {
+            insert_individual_customer
+                .then(result => console.log(result))
+                .catch(error => console.log(error.message));
+        })
+        .catch(error => console.log(error.message));
+
+    return response.status(200).send("No worries");
+});
+
+router.post('/corporate', (request, response) => {
+    const {
+        error
+    } = validateCorporate(request.body);
+    if (error) {
+        return response.status(404).send(error.details[0].message);
+    }
+    return response.status(200).send("No worries");
+});
+
+router.get('/individual', (request, response) => {
+    response.sendFile(path.join(__dirname, '../views/individual.html'));
+});
+
+router.get('/corporate', (request, response) => {
+    response.sendFile(path.join(__dirname, '../views/corporate.html'));
+});
+
+module.exports = router;
+
+function test() {
+    return new Promise((resolve, reject) => {
+        const query = pool.query('SELECT * FROM branch', function (error, results, fields) {
+            if (error) reject(error);
+            else resolve(results);
+        });
+    });
+}
--- startup/routes.js
+++ startup/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const customer = require('../routes/customer');
+const customer = require('../routes/employee_functionalities/customer');
 const savings_account = require('../routes/savings_account');
 
 module.exports = function (app) {
@@ -8,10 +8,21 @@ module.exports = function (app) {
     
     app.use(express.urlencoded({ extended: true }));
 
-    //endpoints added
-    app.use('/customer', customer);
+    //ENDPOINTS
+
+    //Employees
+    // app.use('/employee_functionalities/customer', customer);
+    // app.use('/employee_functionalities/savings_account', savings_account);
+    // app.use('/employee_functionalities/checking_account', checking_account);
+    // app.use('/employee_functionalities/fixed_deposit', fixed_deposit);
+    // app.use('/employee_functionalities/normal_loan', normal_loan);
+
+    //Customers
+
+
+    //Branch Managers
 
     //savings_acc end point
-    app.use('/savings_account',savings_account);
+    
 
 }
