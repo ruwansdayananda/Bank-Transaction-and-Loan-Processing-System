94a6dc1dd496a3d7be6b01b59e385b70c90b0685
Gayangi Seneviratne <sgayangi@gmail.com>
2021-01-10 14:08:53 +0530

    fixed validations

--- models/customer.js
+++ models/customer.js
@@ -5,44 +5,31 @@ function validateIndividual(customer) {
     const cutoffDate = new Date(now - (1000 * 60 * 60 * 24 * 365 * 18));
 
     const schema = Joi.object({
-        "individual_id": Joi.number().integer().min(100001).required(),
-        "full_name": Joi.string().pattern(new RegExp('^[a-z]+(?: [a-z]+)+$')).required().min(5),    //the name must have at least two words seperated by a space
-        "address": Joi.string().required(),
-        "national_ID": Joi.string().required().min(10),
-        "date_of_birth": Joi.date().greater('1974-01-01').less(cutoffDate).required(),
-        "residential_contact_no": Joi.string().required().min(10),
-        "personal_contact_no": Joi.string().required().min(10),
-        "date_joined": Joi.string().required(),
-        "email_address": Joi.string().required(),
-        "password": Joi.string().min(5).max(1024).required(),
+        "full_name"                 : Joi.string().pattern(new RegExp('^[a-z]+(?: [a-z]+)+$')).required().min(5),    //the name must have at least two words seperated by a space
+        "address"                   : Joi.string().required(),
+        "national_ID"               : Joi.string().required().min(10),
+        "date_of_birth"             : Joi.date().greater('1974-01-01').less(cutoffDate).required(),
+        "residential_contact_no"    : Joi.string().required().min(10),
+        "personal_contact_no"       : Joi.string().required().min(10),
+        "date_joined"               : Joi.string().required(),
+        "email"                     : Joi.string().email().required(),
+        "password"                  : Joi.string().min(5).max(1024).required(),
     });
     return schema.validate(customer);
 }
 
 function validateCorporate(company) {
     const schema = Joi.object({
-
-        "corporate_id": Joi.string()
-            // .pattern(new RegExp('^SC.*SCR$')) //this regex means that the customer id has to start with SC and end with SCR with total 15 characters
-            .required().min(20)
-            .messages({         //how to define custom messages
-                'string.base': `"corporate_id" should be text`,
-                'string.empty': `"corporate_id" cannot be an empty field`,
-                'string.regex': `"asdasda`,
-                'string.min': `"corporate_id" should have a minimum length of 15`,
-                'any.required': `"corporate_id" is a required field`
-            }),
-
-        "company_registration_number": Joi.string().required(),
-        "company_name":Joi.string().min(3).required(),
-        "company_email_address": Joi.string().max(319).required(),
-        "address":Joi.string().required(),
-        "date_of_establishment":Joi.date().required(),    //Constranins must be checked
-        "contact_no":Joi.string().required().min(10),
-        "date_joined":Joi.date().required(),     // Constraints must be checked
-        "correspondent":Joi.string().alphanum().required(),
-        "correspondent_email_address":Joi.string().max(319).required(),
-        "password": Joi.string().min(5).max(1024).required(),
+        "company_registration_number"   : Joi.string().required(),
+        "company_name"                  : Joi.string().min(3).required(),
+        "company_email"                 : Joi.string().email().required(),
+        "address"                       : Joi.string().required(),
+        "date_of_establishment"         : Joi.date().required(),    //Constranins must be checked
+        "contact_no"                    : Joi.string().required().min(10),
+        "date_joined"                   : Joi.date().required(),     // Constraints must be checked
+        "correspondent"                 : Joi.string().alphanum().required(),
+        "correspondent_email"           : Joi.string().email().required(),
+        "password"                      : Joi.string().min(5).max(1024).required(),
         
     });
     return schema.validate(company);
