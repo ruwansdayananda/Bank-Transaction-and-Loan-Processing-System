e057037ef1ca166d516350e627e13ff871459a38
binoy <binoy.18@cse.mrt.ac.lk>
2021-01-10 16:05:12 +0530

    fixed creation query

--- models/fixed_deposit.js
+++ models/fixed_deposit.js
@@ -4,11 +4,10 @@ const Joi = require('joi');
 function validateFixedDeposit(account) {
     
     const schema = Joi.object({
-        "fixed_deposit_id": Joi.number().integer().required(),
         "fixed_deposit_plan_id": Joi.number().integer().required(),
         "branch_id": Joi.number().integer().required(),
-        "savings_account_id": Joi.string().required().max(30),
-        "customer_id": Joi.string().required().min(15),
+        "savings_account_id": Joi.number().required(),
+        "customer_id": Joi.number().required(),
         "deposit_amount" : Joi.number().required().positive().precision(2),
         "started_date" : Joi.date().required(),
         
--- routes/employee_functionalities/fixed_deposit.js
+++ routes/employee_functionalities/fixed_deposit.js
@@ -2,15 +2,53 @@ const express = require('express');
 const router = express.Router();
 const {validateFixedDeposit} = require('../../models/fixed_deposit');
 
+const _ = require('lodash');
+const {
+    pool
+} = require('../../startup/mysql_database');
+
 
 // route to create new savings account
-router.post('/create', (request,response)=>{
+router.post('/create', async (request,response)=>{
     const {error} = validateFixedDeposit(request.body);
     if(error){
         return response.status(400).send(error.message);
+    }
+
+    try {
+        await create_fixed_deposit(_.pick(request.body, ["fixed_deposit_plan_id", "branch_id", "savings_account_id", "customer_id","deposit_amount","started_date"]));
+        
+    } catch (error) {
+        response.status(400).send(error.message);
     }
     return response.send(request.body);
 
 });
 
 module.exports = router;
+
+function create_fixed_deposit(body) {
+
+    return new Promise((resolve, reject) => {
+        const result = pool.query("CALL create_fixed_deposit (?,?,?,?,?,?)",
+            [
+                body.fixed_deposit_plan_id,
+                body.branch_id,
+                body.savings_account_id,
+                body.customer_id,
+                body.deposit_amount,
+                body.started_date,
+
+
+            ],
+            function (error, results, fields) {
+                if (error) {
+                    reject(error);
+                };
+                resolve(console.log("succesful"));
+            }
+        )
+    })
+    
+}
+
