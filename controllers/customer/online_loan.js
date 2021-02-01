467a5cd283a4afab763de9266d20d3195197f07e
binoy <binoy.18@cse.mrt.ac.lk>
2021-02-01 23:21:11 +0530

    max loan amount check imp

--- controllers/customer/online_loan.js
+++ controllers/customer/online_loan.js
@@ -1,5 +1,6 @@
 
 const Joi = require('joi');
+const { max } = require('lodash');
 const _ = require('lodash');
 const { render } = require('pug');
 const Customer = require('../../models/Customer');
@@ -33,18 +34,24 @@ const createOnlineLoan = async (request,response) => {
     request.body.branch_id = request.body.fixed_deposit.branch_id;
     request.body.loan_installment = parseFloat(request.body.loan_amount)*interest_rate/1200;
 
-    /**
-     * @todo  check loan amount 500000 or other
-     */
+    let max_loan_amount = Math.min(500000.0,parseFloat(request.body.fixed_deposit.deposit_amount)*0.6);
 
-
-
-    try {
-        await Customer.createOnlineLoan(_.pick(request.body, ["loan_plan_id", "fixed_deposit_id", "customer_id", "branch_id","loan_installment","loan_amount"]));
-    } catch (error) {
-        console.log(error.message);
+    if(parseFloat(request.body.loan_amount) > max_loan_amount){
+        return response.send("ERROR GO BACK");
     }
-    return response.status(200).send(request.body);
+
+    else{
+        try {
+            await Customer.createOnlineLoan(_.pick(request.body, ["loan_plan_id", "fixed_deposit_id", "customer_id", "branch_id","loan_installment","loan_amount"]));
+        } catch (error) {
+            console.log(error.message);
+        }
+        return response.status(200).send(request.body);
+
+    }
+
+
+    
 };
 
 const loadOnlineLoanForm = async (request,response)=>{
